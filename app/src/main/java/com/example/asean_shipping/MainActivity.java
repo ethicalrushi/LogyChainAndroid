bf833c4196c2e806119a0e10fb10c796df8b8ef6
ethicalrushi <pupalerushikesh@gmail.com>
2021-01-31 18:55:17 +0530

    adding shipFrom and shipTo forms

--- app/src/main/java/com/example/asean_shipping/MainActivity.java
+++ app/src/main/java/com/example/asean_shipping/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.asean_shipping.fragments.CreateShipFrom;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent i  = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(i);
+
+        CreateShipFrom createShipFrom = new CreateShipFrom();
+        createShipFrom.show(getSupportFragmentManager(),"createShipFrom");
+//        Intent i  = new Intent(MainActivity.this, LoginActivity.class);
+//        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/asean_shipping/utils/AppConstants.java
@@ -0,0 +1,5 @@
+package com.example.asean_shipping.utils;
+
+public class AppConstants {
+    public static final String BASE_URL = "https://1da24a9b7a5f.ngrok.io/";
+}
