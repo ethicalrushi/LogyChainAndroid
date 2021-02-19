4330a2c7a2f4d9141f83648466fb648ec4df2442
ethicalrushi <pupalerushikesh@gmail.com>
2021-02-19 16:19:27 +0530

    upating constants

--- app/src/main/java/com/example/asean_shipping/utils/AppConstants.java
+++ app/src/main/java/com/example/asean_shipping/utils/AppConstants.java
@@ -1,5 +1,5 @@
 package com.example.asean_shipping.utils;
 
 public class AppConstants {
-    public static final String BASE_URL = "https://8a3852d4816f.ngrok.io/";
+    public static final String BASE_URL = "https://4dbf18775a72.ngrok.io/";
 }
